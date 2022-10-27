212ab48719e93d6f0a32989389f0932c2d9583fe
Azeez <azeezade04@gmail.com>
2022-10-27 14:48:43 +0100

    hng stage 1 initial commit

--- app.js
+++ app.js
@@ -17,14 +17,3 @@ const port = process.env.PORT || 2000;
 app.listen(port, () => {
   console.log(`App started!!! Listening on port ${port}`);
 });
-
-// - Setup a server (Host)
-// - Create an **(GET)** api endoint that returns the following  json response:
-
-//      { "**slackUsername**": String, "**backend**": Boolean, "**age**": Integer, "**bio**": String }
-
-//     - SlackUsername should be a **string** datatype and your slack username
-//     - Backend should be a **boolean** datatype
-//     - Age should be an  **integer** datatype
-//     - Bio(description about yourself) should be a **string** datatype
-// - Push to **GitHub**
